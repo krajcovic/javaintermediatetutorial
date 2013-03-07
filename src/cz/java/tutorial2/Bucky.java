571a6ed211b63796c6b3d9b1856f129813510ffe
Krajcovic <dusan.krajcovic@gmail.com>
2013-03-07 14:56:55 +0100

    Refactoring by checkstyle.

--- src/cz/java/tutorial2/Bucky.java
+++ src/cz/java/tutorial2/Bucky.java
@@ -1,9 +1,10 @@
 /**
- * 
+ * <module name="JavadocType"/>
  */
 package cz.java.tutorial2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.LinkedList;
@@ -14,34 +15,56 @@ import java.util.ListIterator;
  * @author "Dusan Krajcovic"
  * 
  */
-public class Bucky {
+public final class Bucky {
+
+	/**
+	 * Denied create this class.
+	 */
+	private Bucky() {
+
+	}
 
 	/**
 	 * @param args
+	 *            input optional arguments
 	 */
-	public static void main(String[] args) {
+	public static void main(final String[] args) {
 
 		// tutorial01();
 		// tutorial04();
-		tutorial06();
-		// tutorial08();
+		// tutorial06();
+		tutorial08();
 	}
 
+	/**
+	 * Converting lists to arrays.
+	 */
 	private static void tutorial08() {
+		String[] stuff = { "babies", "watermelong", "melons", "fudge" };
+		LinkedList<String> thelist = new LinkedList<String>(
+				Arrays.asList(stuff));
 
+		thelist.add("pumpinkinf");
+		thelist.add("");
 	}
 
+	/**
+	 * LinkedList.
+	 */
+	@SuppressWarnings("unused")
 	private static void tutorial06() {
 
 		String[] things = { "apples", "noobs", "pwnge", "bacon", "goATS" };
 		List<String> list = new LinkedList<String>();
-		for (String x : things)
+		for (String x : things) {
 			list.add(x);
+		}
 
 		String[] things2 = { "sausage", "baocn", "goats", "harrypotter" };
 		List<String> list2 = new LinkedList<>();
-		for (String x : things2)
+		for (String x : things2) {
 			list2.add(x);
+		}
 
 		list.addAll(list2);
 		list2 = null;
@@ -53,7 +76,13 @@ public class Bucky {
 
 	}
 
-	private static void reverseMe(List<String> list) {
+	/**
+	 * Print to console strings from list in reverse order.
+	 * 
+	 * @param list
+	 *            with data
+	 */
+	private static void reverseMe(final List<String> list) {
 		ListIterator<String> it = list.listIterator(list.size());
 		while (it.hasPrevious()) {
 			System.out.printf("%s ", it.previous());
@@ -61,13 +90,30 @@ public class Bucky {
 
 	}
 
-	private static void removeStuff(List<String> list, int i, int j) {
+	/**
+	 * Remove stuff from list from to.
+	 * 
+	 * @param list
+	 *            with strings
+	 * @param from
+	 *            remove from
+	 * @param to
+	 *            remove to
+	 */
+	private static void removeStuff(final List<String> list, final int from,
+			final int to) {
 
-		list.subList(i, j).clear();
+		list.subList(from, to).clear();
 
 	}
 
-	private static void printMe(List<String> list) {
+	/**
+	 * Print to standards console a strings from list.
+	 * 
+	 * @param list
+	 *            with strings.
+	 */
+	private static void printMe(final List<String> list) {
 		for (String string : list) {
 			System.out.printf("%s ", string);
 		}
@@ -76,6 +122,10 @@ public class Bucky {
 
 	}
 
+	/**
+	 * Introduction to collections.
+	 */
+	@SuppressWarnings("unused")
 	private static void tutorial04() {
 		String[] things = { "eggs", "lasers", "hats", "pie" };
 		List<String> thingsList = new ArrayList<String>();
@@ -111,7 +161,16 @@ public class Bucky {
 
 	}
 
-	private static void editlist(Collection<String> l1, Collection<String> l2) {
+	/**
+	 * Remove strings from l1 if are in l2.
+	 * 
+	 * @param l1
+	 *            The checked list with strings.
+	 * @param l2
+	 *            The control list with strings.
+	 */
+	private static void editlist(final Collection<String> l1,
+			final Collection<String> l2) {
 		Iterator<String> it = l1.iterator();
 		while (it.hasNext()) {
 			if (l2.contains(it.next())) {
@@ -121,6 +180,10 @@ public class Bucky {
 
 	}
 
+	/**
+	 * Common string methods.
+	 */
+	@SuppressWarnings("unused")
 	private static void tutorial01() {
 		ArrayList<String> words = new ArrayList<>();
 		words.add("funk");
